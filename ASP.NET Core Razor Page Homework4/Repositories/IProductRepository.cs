1a6ad57262b457a9b1fa34534927e49d0e5e838f
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-07-31 13:05:09 +0400

    Add project files.

---
+++ ASP.NET Core Razor Page Homework4/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ASP.NET_Core_Razor_Page_Homework4.Entities;
+
+namespace ASP.NET_Core_Razor_Page_Homework4.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAllAsync();
+        Product GetByIdAsync(int id);
+        void AddAsync(Product product);
+        void UpdateAsync(Product product);
+        void DeleteAsync(int id);
+    }
+}
---
+++ ASP.NET Core Razor Page Homework4/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ASP.NET_Core_Razor_Page_Homework4.Entities;
+
+namespace ASP.NET_Core_Razor_Page_Homework4.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetAllAsync();
+        void AddAsync(Product product);
+        void UpdateAsync(Product product);
+        void DeleteAsync(int id);
+        Product GetByIdAsync(int id);
+    }
+}
